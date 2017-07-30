9b3cb95bfbc192846abb60a7629d386ae0d016f8
Bob Lail <bob.lailfamily@gmail.com>
2017-07-30 15:04:38 -0500

    [skip] Used the social login avatar rather than gravatar when known (10m)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,21 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(access_token)
     info = access_token.info
-    User.create_with({
-      name: info.fetch("name"),
-    }).find_or_create_by(email: info.fetch("email"))
+    user = User.find_or_initialize_by(email: info.fetch("email"))
+    user.name = info.fetch("name")
+    user.image_url = info.fetch("image")
+    user.save! if user.changed?
+    user
+  end
+
+  def image_url(options={})
+    super() || gravatar_url(options)
+  end
+
+  def gravatar_url(options={})
+    url = "//www.gravatar.com/avatar/#{Digest::MD5::hexdigest(email)}?r=g&d=retro"
+    url << "&s=#{options[:size] * 2}" if options.key?(:size)
+    url
   end
 
   def family_members
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730194632) do
+ActiveRecord::Schema.define(version: 20170730195242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170730194632) do
     t.datetime "updated_at"
     t.string   "name",                            null: false
     t.string   "family_members",     default: [],              array: true
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
