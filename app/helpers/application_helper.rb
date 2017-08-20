51eefea64cc09477116b8d3cac52dfef6c2e4946
Bob Lail <bob.lailfamily@gmail.com>
2017-08-20 17:14:48 -0500

    [skip] Fixed some CSS glitches (5m)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,11 @@ module ApplicationHelper
 
   def radio_button(name, value, label, model=nil, options={})
     <<~HTML.html_safe
-      #{radio_button_tag name, value, value == model, options}
-      <label for="#{name}_#{value}" class="radio-button"></label>
-      <label for="#{name}_#{value}">#{label}</label>
+      <label for="#{name}_#{value}">
+        #{radio_button_tag name, value, value == model, options}
+        <label for="#{name}_#{value}" class="radio-button"></label>
+        #{label}
+      </label>
     HTML
   end
 
