039de9861f320a10602e7b4ce3d795d1e61998ba
Bob Lail <bob.lailfamily@gmail.com>
2019-03-03 15:54:25 -0600

    [feature] Add a Chrome extension for the Recipes app (30m)

---
+++ platforms/chrome/add-to-lailrecipes/background.js
@@ -0,0 +1,4 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  let url = `https://lailrecipes.herokuapp.com/recipes/new?url=${encodeURIComponent(tab.url)}`;
+  chrome.tabs.create({ url });
+});
