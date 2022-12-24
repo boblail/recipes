b3460b53d6b1b7731254a1c7d1b1220bf6ccc724
Bob Lail <bob.lailfamily@gmail.com>
2022-12-23 16:22:32 -0800

    chore: Upgrade to Rails 6

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Recipes
     config.load_defaults 5.2
     Ingreedy.preserve_amounts = true
 
+    # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
